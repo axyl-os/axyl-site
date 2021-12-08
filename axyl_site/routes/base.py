b995ec73096a104e42547680015b8f63ba237392
Angelo Fallaria <ba.fallaria@gmail.com>
2021-12-08 20:43:09 +0800

    feat: improve the html/css

--- axyl_site/routes/base.py
+++ axyl_site/routes/base.py
@@ -6,4 +6,9 @@ bp = Blueprint("base", __name__)
 
 @bp.route("/")
 def index() -> str:
-    return render_template("index.html")
+    return render_template("index.html", css="landing-page.css")
+
+
+@bp.route("/about")
+def about() -> str:
+    return render_template("about.html", css="about.css")
--- tests/test_app.py
+++ tests/test_app.py
@@ -8,3 +8,12 @@ def test_index():
 
     assert response.status_code == 200
     assert "Welcome to Axyl" in str(response.data)
+
+
+def test_about():
+    app = create_app()
+    tester = app.test_client()
+    response = tester.get("/about", content_type="html/text")
+
+    assert response.status_code == 200
+    assert "About" in str(response.data)
